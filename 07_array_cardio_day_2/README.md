0385e47d63016a17cf21cd3ad2685c6bf15f7008
Taddy <anhdt15@topica.edu.vn>
2018-11-14 10:42:05 +0700

    updated js file

--- 07_array_cardio_day_2/README.md
+++ 07_array_cardio_day_2/README.md
@@ -8,11 +8,28 @@
 
 - Check for these information:
   
-  1. [***some***](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some).
-  2. [***every***](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every)
+  1. [***some***](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some) : return **true** if the callback function return a truthy value for any array element, otherwise, **false**
 
-  3. [***find***](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find)
+  2. [***every***](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every) : return **true**
+  if the callback function return a truthy value for every array element, otherwise, **false**
 
-  4. [***findIndex***](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex)
+  3. [***find***](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find) : return the value of first element in the array that satisfies the provided testing function, otherwise, **undefined** is return
+
+  4. [***findIndex***](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex): retrun an index in the array if an element passes the test, otherwise, return -1
 
   5. [***spread operator***](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax)
+
+     *notice* : in exercise, we can see these lines of code
+
+     ```javascript
+     const newComments = [
+       ...comments.slice(0, index),
+       ...comments.slice(index + 1)
+     ];
+     ```
+
+     if we don't put these dots before array, newComments will be come
+
+     ```javascript
+     newComments = [[], []]
+     ```
--- 07_array_cardio_day_2/main.js
+++ 07_array_cardio_day_2/main.js
@@ -14,40 +14,66 @@
     ];
     // Some and Every Checks
     // Array.prototype.some() // is at least one person 19 or older?
-    const somePeople = people.some(person => {
+    const somePeople = people.some(function(person) {
       const now = new Date();
       const yearOfNow = now.getFullYear();
       if (yearOfNow - person.year >= 19) {
         return true;
       }
     })
-    console.log(somePeople);
+    console.log(`somePeople: ${somePeople}`);
+    // more shorter
+    const somePeople2 = people.some( person => ((new Date()).getFullYear() - person.year) >= 19);
+    console.log(`somePeople2: ${somePeople2}`);
 
     // Array.prototype.every() // is everyone 19 or older?
     const everyPeople = people.every(person => {
       const now = new Date();
       const yearOfNow = now.getFullYear();
-      if (!(yearOfNow - person.year < 19)) {
-        return false;
+      if (yearOfNow - person.year >= 19) {
+        return true;
       }
-      return true;
     })
-    console.log(everyPeople);
+    console.log(`everyPeople: ${everyPeople}`);
+    //more shorter
+    const everyPeople2 = people.every(person => ((new Date()).getFullYear() - person.year) >= 19);
+    console.log(`everyPeople2: ${everyPeople2}`);
 
     // Array.prototype.find()
     // Find is like filter, but instead returns just the one you are looking for
     // find the comment with the ID of 823423
-    const findComments = comments.find(comment => {
-      return comment.id === 823423;
-    })
-    console.log(findComments);
+    const findComments = comments.find(function(comment) {
+      if (comment.id === 823423) {
+        return comment;
+      }
+    });
+    console.log(`findComments: ${JSON.stringify(findComments)}`);
+    //more shorter
+    const findComments2 = comments.find(comment => comment.id === 823423);
+    console.log(`findComments2: ${JSON.stringify(findComments2)}`);
     
     // Array.prototype.findIndex()
     // Find the comment with this ID
-    const findIndex = comments.findIndex(comment => {
-      return comment.id === 823423;
-    })
-    console.log(findIndex);
+    const findIndex = comments.findIndex(function(comment, index) {
+      if (comment.id === 823423) {
+        return index;
+      }
+    });
+    console.log(`findIndex: ${findIndex}`);
+    //more shorter
+    const findIndex2 = comments.findIndex(comment => comment.id === 823423);
+    console.log(`findIndex2: ${findIndex2}`);
+
     // delete the comment with the ID of 823423
+    const newComments = [
+      ...comments.slice(0, findIndex),
+      ...comments.slice(findIndex + 1)
+    ];
+    console.table(newComments);
+
+    //another way
     comments.splice(findIndex, 1);
     console.table(comments);
+        
+    
+
